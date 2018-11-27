efa207c639e30da1c4acb0d583ed0cae72c6180b
Pierre <pierre.baron37@orange.fr>
2018-11-27 17:34:09 +0100

    ajout des variables du jeu

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+// Random function (computer choice)
 
 function getComputerChoice() {
   const choices = ['r', 'p', 's'];
@@ -14,12 +16,52 @@ function getComputerChoice() {
   return choices [randomNumber];
 }
 
+function convertToWord(letter){
+  if (letter === "r") return "La pierre";
+  if (letter === "p") return "La feuille";
+  if (letter === "s") return "Le(s) ciseaux";
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  result_p.innerHTML = convertToWord(userChoice) + " bat " + convertToWord(computerChoice) + ". Tu as gagné !"
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = convertToWord(userChoice) + " est battue " + convertToWord(computerChoice) + ". Tu as perdu !"
+}
+
+function draw(userChoice, computerChoice) {
+  result_p.innerHTML = "Égalité, recommences !"
+}
+
+// The game
+
 function game(userChoice) {
   const computerChoice = getComputerChoice();
-  console.log("computer choice =>" + computerChoice);
-  console.log("user choice =>" + userChoice);
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
 
-} 
+// User choices
 
 function main() {
   rock_div.addEventListener('click', function(){
